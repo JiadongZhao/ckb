f75f4899e78265eb3cc0de3e8b1db8a65f3c7c29
quake <quake.wang@gmail.com>
2020-09-02 10:29:59 +0900

    feat: add get_transaction_proof / verify_transaction_proof rpc

--- test/src/main.rs
+++ test/src/main.rs
@@ -392,6 +392,7 @@ fn all_specs() -> SpecMap {
         Box::new(TemplateTxSelect),
         Box::new(BlockSyncRelayerCollaboration),
         Box::new(RpcTruncate),
+        Box::new(RpcTransactionProof),
         Box::new(SyncTooNewBlock),
         Box::new(RelayTooNewBlock),
         Box::new(LastCommonHeaderForPeerWithWorseChain),
